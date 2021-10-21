14078308e3281d0c18b766b87f3eae97e309d7ca
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-10-21 18:26:22 +0100

    single plotter class

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -1,177 +1,166 @@
+import math
 import os
-from typing import List, Union
+from typing import List, Tuple, Union
 
-import matplotlib as mpl
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib import cm
-
-TIME_UNIT = "time_unit"
-SUMMARY_FIGSIZE = (9, 6)
+from plotter import plotting_functions
 
 
-def smooth_data(data: List[float], window_width: int) -> List[float]:
-    """Calculates moving average of list of values
+class Plotter:
+    """Class for plotting scalar data."""
 
-    Args:
-        data: raw, un-smoothed data.
-        window_width: width over which to take moving averags
-
-    Returns:
-        smoothed_values: averaged data
-    """
-
-    def _smooth(single_dataset):
-        cumulative_sum = np.cumsum(single_dataset, dtype=np.float32)
-        cumulative_sum[window_width:] = (
-            cumulative_sum[window_width:] - cumulative_sum[:-window_width]
-        )
-        # explicitly forcing data type to 32 bits avoids floating point errors
-        # with constant dat.
-        smoothed_values = np.array(
-            cumulative_sum[window_width - 1 :] / window_width, dtype=np.float32
-        )
-        return smoothed_values
-
-    if all(isinstance(d, list) for d in data):
-        smoothed_data = []
-        for dataset in data:
-            smoothed_data.append(_smooth(dataset))
-    elif all(
-        (isinstance(d, float) or isinstance(d, int) or isinstance(d, np.int64))
-        for d in data
-    ):
-        smoothed_data = _smooth(data)
-
-    return smoothed_data
-
-
-def plot_multi_seed_multi_run(
-    folder_path: str,
-    exp_names: List[str],
-    window_width: int,
-    linewidth: int = 3,
-    colormap: Union[str, None] = None,
-):
-    """Expected structure of folder_path is:
-
-    - folder_path
-        |_ run_1
-        |   |_ seed_0
-        |   |_ seed_1
-        |   |_ ...
-        |   |_ seed_M
-        |
-        |_ run_2
-        |   |_ seed_0
-        |   |_ seed_1
-        |   |_ ...
-        |   |_ seed_M
-        |
-        |_ ...
-        |_ ...
-        |_ run_N
-            |_ seed_0
-            |_ seed_1
-            |_ ...
-            |_ seed_M
-
-    with a file called data_logger.csv in each leaf folder.
-    """
-    experiment_folders = {
-        exp_name: os.path.join(folder_path, exp_name) for exp_name in exp_names
+    GRAPH_LAYOUTS = {
+        1: (1, 1),
+        2: (2, 1),
+        3: (3, 1),
+        4: (2, 2),
+        5: (3, 2),
+        6: (3, 2),
+        7: (3, 3),
+        8: (3, 3),
+        9: (3, 3),
+        10: (4, 3),
+        11: (4, 3),
+        12: (4, 3),
     }
 
-    tag_set = {}
+    PLOT_PDF = "plot.pdf"
+    RAW_PLOT_PDF = "raw_plot.pdf"
 
-    # arbitrarily select one seed's dataframe for each run to find set of column names
-    for exp, exp_path in experiment_folders.items():
-        ex_seed = [
-            f for f in os.listdir(exp_path) if os.path.isdir(os.path.join(exp_path, f))
-        ][0]
+    def __init__(
+        self, save_folder: str, logfile_path: str, smoothing: int, xlabel: str
+    ):
+        self._save_folder = save_folder
+        self._logfile_path = logfile_path
+        self._smoothing = smoothing
+        self._xlabel = xlabel
 
-        ex_df = pd.read_csv(os.path.join(exp_path, ex_seed, "data_logger.csv"))
-        tag_subset = list(ex_df.columns)
-        for tag in tag_subset:
-            if tag not in tag_set:
-                tag_set[tag] = []
-            tag_set[tag].append(exp)
+        self._plot_tags: List[str]
 
-    if colormap is not None:
-        cmap = cm.get_cmap(colormap)
+        self._log_df: pd.DataFrame
+        self._scaling: int
 
-        assert isinstance(
-            cmap, mpl.colors.ListedColormap
-        ), "colormap must be discrete, i.e. ListedColormap."
+    def load_data(self) -> None:
+        """Read in data logged to path."""
+        self._log_df = pd.read_csv(self._logfile_path)
+        self._plot_tags = list(self._log_df.columns)
+        self._scaling = len(self._log_df)
 
-        color_cycle = mpl.cycler(color=cmap.colors)
-        mpl.rcParams["axes.prop_cycle"] = color_cycle
+    @staticmethod
+    def get_figure_skeleton(
+        height: Union[int, float],
+        width: Union[int, float],
+        num_columns: int,
+        num_rows: int,
+    ) -> Tuple:
 
-    for tag, relevant_experiments in tag_set.items():
-        if tag not in []:
-            print(tag)
+        fig = plt.figure(
+            constrained_layout=False, figsize=(num_columns * width, num_rows * height)
+        )
 
-            fig = plt.figure(figsize=(SUMMARY_FIGSIZE))
-            for exp in relevant_experiments:
-                attribute_data = []
-                seed_folders = [
-                    f
-                    for f in os.listdir(experiment_folders[exp])
-                    if os.path.isdir(os.path.join(experiment_folders[exp], f))
-                ]
+        heights = [height for _ in range(num_rows)]
+        widths = [width for _ in range(num_columns)]
 
-                for seed in seed_folders:
-                    df = pd.read_csv(
-                        os.path.join(experiment_folders[exp], seed, "data_logger.csv")
+        spec = gridspec.GridSpec(
+            nrows=num_rows,
+            ncols=num_columns,
+            width_ratios=widths,
+            height_ratios=heights,
+        )
+
+        return fig, spec
+
+    def plot_learning_curves(self):
+        # unsmoothed
+        self._plot_learning_curves(smoothing=None)
+        # smoothed
+        self._plot_learning_curves(smoothing=self._smoothing)
+
+    def _plot_learning_curves(self, smoothing: int) -> None:
+
+        num_graphs = len(self._plot_tags)
+
+        default_layout = (
+            math.ceil(np.sqrt(num_graphs)),
+            math.ceil(np.sqrt(num_graphs)),
+        )
+        graph_layout = self.GRAPH_LAYOUTS.get(num_graphs, default_layout)
+
+        num_rows = graph_layout[0]
+        num_columns = graph_layout[1]
+
+        self.fig, self.spec = self.get_figure_skeleton(
+            height=4, width=5, num_columns=num_columns, num_rows=num_rows
+        )
+
+        for row in range(num_rows):
+            for col in range(num_columns):
+
+                graph_index = (row) * num_columns + col
+
+                if graph_index < num_graphs:
+
+                    print("Plotting graph {}/{}".format(graph_index + 1, num_graphs))
+                    self._plot_scalar(
+                        row=row,
+                        col=col,
+                        data_tag=self._plot_tags[graph_index],
+                        smoothing=smoothing,
                     )
-                    tag_data = df[tag].dropna()
-                    attribute_data.append(tag_data)
 
-            if len(attribute_data):
-                mean_attribute_data = np.mean(attribute_data, axis=0)
-                std_attribute_data = np.std(attribute_data, axis=0)
-                smooth_mean_data = smooth_data(
-                    mean_attribute_data, window_width=window_width
+        if smoothing is not None:
+            save_path = os.path.join(self._save_folder, self.PLOT_PDF)
+        else:
+            save_path = os.path.join(self._save_folder, self.RAW_PLOT_PDF)
+        plt.tight_layout()
+        self.fig.savefig(save_path, dpi=100)
+        plt.close()
+
+    def _plot_scalar(self, row: int, col: int, data_tag: str, smoothing: int):
+        fig_sub = self.fig.add_subplot(self.spec[row, col])
+
+        # labelling
+        fig_sub.set_xlabel(self._xlabel)
+        fig_sub.set_ylabel(data_tag)
+
+        # grids
+        fig_sub.minorticks_on()
+        fig_sub.grid(
+            which="major", linestyle="-", linewidth="0.5", color="red", alpha=0.2
+        )
+        fig_sub.grid(
+            which="minor", linestyle=":", linewidth="0.5", color="black", alpha=0.4
+        )
+
+        # plot data
+        if data_tag in self._log_df.columns:
+            sub_fig_tags = [data_tag]
+            sub_fig_data = [self._log_df[data_tag].dropna()]
+        else:
+            sub_fig_tags = [tag for tag in self._log_df.columns if data_tag in tag]
+            sub_fig_data = [self._log_df[tag].dropna() for tag in sub_fig_tags]
+
+        if smoothing is not None:
+            smoothed_data = [
+                plotting_functions.smooth_data(
+                    data=data.to_numpy(), window_width=min(len(data), smoothing)
                 )
-                smooth_std_data = smooth_data(
-                    std_attribute_data, window_width=window_width
-                )
-                if len(smooth_mean_data):
-                    scaled_x = (len(df) / len(smooth_mean_data)) * np.arange(
-                        len(smooth_mean_data)
-                    )
-                    plt.plot(
-                        scaled_x,
-                        smooth_mean_data,
-                        linewidth=linewidth,
-                        label=exp,
-                    )
-                    plt.fill_between(
-                        scaled_x,
-                        smooth_mean_data - smooth_std_data,
-                        smooth_mean_data + smooth_std_data,
-                        alpha=0.3,
-                    )
-            plt.legend(
-                bbox_to_anchor=(
-                    1.01,
-                    1.0,
-                ),
-                loc="upper left",
-                ncol=1,
-                borderaxespad=0.0,
-            )
-            plt.xlabel(TIME_UNIT)
-            plt.ylabel(tag)
+                for data in sub_fig_data
+            ]
+        else:
+            smoothed_data = sub_fig_data
 
-            fig.tight_layout()
+        x_data = [
+            (self._scaling / len(data)) * np.arange(len(data)) for data in smoothed_data
+        ]
 
-            os.makedirs(os.path.join(folder_path, "figures"), exist_ok=True)
-            fig.savefig(
-                os.path.join(
-                    folder_path, "figures", f"{tag}_plot_multi_seed_multi_run.pdf"
-                ),
-                dpi=100,
-            )
-            plt.close()
+        for x, y, label in zip(x_data, smoothed_data, sub_fig_tags):
+            fig_sub.plot(x, y, label=label)
+
+        # fig_sub.legend()
+        row_index = row + 1
+        title = f"{str(col + 1)}{chr(ord('`')+row_index)}"
+        fig_sub.set_title(title)
