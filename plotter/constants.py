4aa7354b7c4147f400e4f90b771ab5b7484c11bb
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-11-01 20:46:04 +0000

    initial constants commit

---
+++ plotter/constants.py
@@ -0,0 +1,22 @@
+TIME_UNIT = "time_unit"
+SUMMARY_FIGSIZE = (9, 6)
+DISCRETE = "discrete"
+CONTINUOUS = "continuous"
+
+PLOT_PDF = "plot.pdf"
+RAW_PLOT_PDF = "raw_plot.pdf"
+
+GRAPH_LAYOUTS = {
+    1: (1, 1),
+    2: (2, 1),
+    3: (3, 1),
+    4: (2, 2),
+    5: (3, 2),
+    6: (3, 2),
+    7: (3, 3),
+    8: (3, 3),
+    9: (3, 3),
+    10: (4, 3),
+    11: (4, 3),
+    12: (4, 3),
+}
