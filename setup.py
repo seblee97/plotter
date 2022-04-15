f557dda1520ddee4f05e914cae4d8f2319aa518f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-04-15 20:41:52 +0100

    scipy requirement added

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["matplotlib", "pandas", "numpy"],
+    install_requires=["matplotlib", "pandas", "numpy", "scipy"],
 )
